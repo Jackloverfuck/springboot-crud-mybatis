ce9a800fa695dfbf935960cad8097bc4f32dfa19
springboot-crud <930265645@qq.com>
2020-07-28 17:17:00 +0800

    员工页面添加

--- src/main/java/com/zhou/SpringbootCrudMybatisApplication.java
+++ src/main/java/com/zhou/SpringbootCrudMybatisApplication.java
@@ -9,6 +9,17 @@ public class SpringbootCrudMybatisApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootCrudMybatisApplication.class, args);
+
+        Integer a = 100;
+        Integer b = 100;
+        Integer c = 200;
+        Integer d = 200;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(a==b);
+        System.out.println(c==d);
     }
 
 }
