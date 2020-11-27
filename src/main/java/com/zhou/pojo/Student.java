94bd6fdce7bb12e59a103d4cde70e92c507a1794
ZLK <930265645@qq.com>
2020-11-27 22:47:48 +0800

    合并分支测试

---
+++ src/main/java/com/zhou/pojo/Student.java
@@ -0,0 +1,13 @@
+package com.zhou.pojo;
+
+public class Student {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
